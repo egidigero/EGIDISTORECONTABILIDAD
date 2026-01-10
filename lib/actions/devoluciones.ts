232a5b38ea04a69d3a2dcf06e429743d08b9fd65
egidigero <gegidi@udesa.edu.ar>
2026-01-09 21:43:49 -0300

    .

--- lib/actions/devoluciones.ts
+++ lib/actions/devoluciones.ts
@@ -1175,6 +1175,7 @@ export async function updateDevolucion(id: string, data: Partial<DevolucionFormD
       // Debug: log attempted update payload to help diagnose why updates fail
       const snakeCasePayload = toSnakeCase(validatedMergedPartial)
       try { console.debug('[updateDevolucion] BEFORE filter, snakeCasePayload.fecha_recepcion=', snakeCasePayload.fecha_recepcion, 'validatedMergedPartial.fechaRecepcion=', validatedMergedPartial.fechaRecepcion) } catch (dbg) {}
+      try { console.debug('[updateDevolucion] snakeCasePayload.fecha_completada=', snakeCasePayload.fecha_completada, 'type=', typeof snakeCasePayload.fecha_completada, 'validatedMergedPartial.fechaCompletada=', validatedMergedPartial.fechaCompletada) } catch (dbg) {}
       // Filtrar undefined para evitar enviar campos que no queremos actualizar
       const cleanPayload = Object.fromEntries(
         Object.entries(snakeCasePayload).filter(([_, v]) => v !== undefined)
