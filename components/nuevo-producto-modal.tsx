715cd5056de20e861d67b9542c3b0e3c325cd29d
egidigero <gegidi@udesa.edu.ar>
2026-01-19 12:59:49 -0300

    .

--- components/nuevo-producto-modal.tsx
+++ components/nuevo-producto-modal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react"
 import { ProductoForm } from "./producto-form"
 
--- components/producto-form.tsx
+++ components/producto-form.tsx
@@ -1,6 +1,8 @@
-
 import React from "react"
 "use client"
+
+"use client"
+import React from "react"
 
 import { useState } from "react"
 import { useRouter } from "next/navigation"
