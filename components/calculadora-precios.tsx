4206c1b7c4edcef82327be8e291e08ca6ffcaf12
egidigero <gegidi@udesa.edu.ar>
2026-02-02 09:30:18 -0300

    -

--- components/calculadora-precios.tsx
+++ components/calculadora-precios.tsx
@@ -1,6 +1,6 @@
 ﻿"use client"
 
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
